6db64b0dd643e6d08b18ac617db76ce53c5e836a
Cuong Phan <pcuong@umich.email>
2021-03-09 13:44:27 -0500

    edit the second time

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,9 +2,9 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 15;
-        int second = 25;
-        int third = 10;
+        int first = 5;
+        int second = 5;
+        int third = 1;
 
         System.out.println("Enter two numbers: " + first + " " + second + " " +third);
         int sum = first + second;
